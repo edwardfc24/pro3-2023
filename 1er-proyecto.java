5a9c9e6c306cc7dca2331c96238ad1ab1acb3b53
LeonardoLv21 <Devllv21_@hotmail.com>
2023-04-08 01:12:06 -0400

    tarea2-mandelbrot

--- 1er-proyecto.java
+++ 1er-proyecto.java
@@ -1,165 +1,165 @@
-
-/// ordenamiento QuickSort
-
-public class QuickSort {
-    private Panel panel;
-    private int[] arreglo;
-    private int length;
-
-    public QuickSort(Panel fr) {
-        this.panel = fr;
-    }
-
-    public void quickSort(int[] array) {
-        if (array == null || array.length == 0) {
-            return;
-        }
-        this.arreglo = array;
-        length = array.length;
-        quickSort(0, length - 1);
-
-    }
-
-    private void quickSort(int indiceMenor, int indiceMayor) {
-        int i = indiceMenor;
-        int j = indiceMayor;
-        int pivot = arreglo[indiceMenor + (indiceMayor - indiceMenor) / 2];
-        while (i <= j) {
-            while (arreglo[i] < pivot) {
-                i++;
-            }
-            while (arreglo[j] > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                intercambiar(i, j);
-                i++;
-                j--;
-            }
-        }
-        if (indiceMenor < j)
-            quickSort(indiceMenor, j);
-        if (i < indiceMayor)
-            quickSort(i, indiceMayor);
-    }
-
-    private void intercambiar(int i, int j) {
-        int temp = arreglo[i];
-        arreglo[i] = arreglo[j];
-        arreglo[j] = temp;
-        panel.setNumeros(arreglo);
-    }
-}
-
-
-// ordenamiento bubblesort
-
-public class BubbleSort {
-
-    private Panel panel;
-
-    public BubbleSort(Panel fr) {
-        this.panel = fr;
-    }
-
-    public void metodoBubbleSort(int[] arreglo) {
-
-        int temporal;
-        for (int i = 0; i <arreglo.length ; i++) {
-            for (int j = 1; j < (arreglo.length - i); j++) {
-                if (arreglo[j - 1] > arreglo[j]) {
-                    temporal = arreglo[j - 1];//0= 4  4=4
-                    arreglo[j - 1] = arreglo[j]; // 4 = 7 == 7 =7
-                    arreglo[j] = temporal; // 7 =4 4 =4
-                    panel.setNumeros(arreglo);
-                }
-            }
-        }
-    }
-}
-
-
-
-//clase panel
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.Arrays;
-
-public class Panel extends JPanel implements Runnable {
-
-    private int  ancho = 50;
-    private int[] numeros = {6,11,-2,5,7,-7,1,28,10};     //  -7 -2 1 5 6 7 10 11 28
-    private BubbleSort bubble = new BubbleSort(this);
-    private QuickSort quick = new QuickSort(this);
-
-    public Panel() {
-        setLayout(null);
-        this.setSize(600, 500);
-        this.setPreferredSize(getSize());
-        //llenarArray();
-    }
-
-
-    public void dibujarArray(Graphics g, int x, int y) {
-
-        for (Integer num: numeros) {
-            g.drawRect(x, y, ancho, num);
-            x += ancho + 10;
-        }
-    }
-
-
-    @Override
-    public void run() {
-        quick.quickSort(numeros);
-        //bubble.bubbleSort(numeros);
-    }
-
-    public void setNumeros(int[] numeros) {
-        this.numeros = numeros;
-        repaint();
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(Arrays.toString(numeros));
-    }
-
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        dibujarArray(g, 20, 200);
-    }
-
-}
-
-
-///Clase principal
-
-import javax.swing.*;
-public class Main extends JFrame {
-    private JPanel panel = new Panel();
-    public Main() {
-        init();
-        Thread hilo = new Thread((Runnable) panel);
-        hilo.start();
-    }
-    private void init() {
-        setLayout(null);
-        this.setSize(600,500);
-        this.setTitle("Primera Tarea");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.getContentPane().add(panel);
-        this.setLocationRelativeTo(null);
-    }
-
-    public static void main(String[] args) {
-        Main main = new Main();
-        main.setVisible(true);
-    }
-}
+//
+///// ordenamiento QuickSort
+//
+//public class QuickSort {
+//    private Panel panel;
+//    private int[] arreglo;
+//    private int length;
+//
+//    public QuickSort(Panel fr) {
+//        this.panel = fr;
+//    }
+//
+//    public void quickSort(int[] array) {
+//        if (array == null || array.length == 0) {
+//            return;
+//        }
+//        this.arreglo = array;
+//        length = array.length;
+//        quickSort(0, length - 1);
+//
+//    }
+//
+//    private void quickSort(int indiceMenor, int indiceMayor) {
+//        int i = indiceMenor;
+//        int j = indiceMayor;
+//        int pivot = arreglo[indiceMenor + (indiceMayor - indiceMenor) / 2];
+//        while (i <= j) {
+//            while (arreglo[i] < pivot) {
+//                i++;
+//            }
+//            while (arreglo[j] > pivot) {
+//                j--;
+//            }
+//            if (i <= j) {
+//                intercambiar(i, j);
+//                i++;
+//                j--;
+//            }
+//        }
+//        if (indiceMenor < j)
+//            quickSort(indiceMenor, j);
+//        if (i < indiceMayor)
+//            quickSort(i, indiceMayor);
+//    }
+//
+//    private void intercambiar(int i, int j) {
+//        int temp = arreglo[i];
+//        arreglo[i] = arreglo[j];
+//        arreglo[j] = temp;
+//        panel.setNumeros(arreglo);
+//    }
+//}
+//
+//
+//// ordenamiento bubblesort
+//
+//public class BubbleSort {
+//
+//    private Panel panel;
+//
+//    public BubbleSort(Panel fr) {
+//        this.panel = fr;
+//    }
+//
+//    public void metodoBubbleSort(int[] arreglo) {
+//
+//        int temporal;
+//        for (int i = 0; i <arreglo.length ; i++) {
+//            for (int j = 1; j < (arreglo.length - i); j++) {
+//                if (arreglo[j - 1] > arreglo[j]) {
+//                    temporal = arreglo[j - 1];//0= 4  4=4
+//                    arreglo[j - 1] = arreglo[j]; // 4 = 7 == 7 =7
+//                    arreglo[j] = temporal; // 7 =4 4 =4
+//                    panel.setNumeros(arreglo);
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//
+//
+////clase panel
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.util.Arrays;
+//
+//public class Panel extends JPanel implements Runnable {
+//
+//    private int  ancho = 50;
+//    private int[] numeros = {6,11,-2,5,7,-7,1,28,10};     //  -7 -2 1 5 6 7 10 11 28
+//    private BubbleSort bubble = new BubbleSort(this);
+//    private QuickSort quick = new QuickSort(this);
+//
+//    public Panel() {
+//        setLayout(null);
+//        this.setSize(600, 500);
+//        this.setPreferredSize(getSize());
+//        //llenarArray();
+//    }
+//
+//
+//    public void dibujarArray(Graphics g, int x, int y) {
+//
+//        for (Integer num: numeros) {
+//            g.drawRect(x, y, ancho, num);
+//            x += ancho + 10;
+//        }
+//    }
+//
+//
+//    @Override
+//    public void run() {
+//        quick.quickSort(numeros);
+//        //bubble.bubbleSort(numeros);
+//    }
+//
+//    public void setNumeros(int[] numeros) {
+//        this.numeros = numeros;
+//        repaint();
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println(Arrays.toString(numeros));
+//    }
+//
+//
+//    @Override
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//
+//        dibujarArray(g, 20, 200);
+//    }
+//
+//}
+//
+//
+/////Clase principal
+//
+//import javax.swing.*;
+//public class Main extends JFrame {
+//    private JPanel panel = new Panel();
+//    public Main() {
+//        init();
+//        Thread hilo = new Thread((Runnable) panel);
+//        hilo.start();
+//    }
+//    private void init() {
+//        setLayout(null);
+//        this.setSize(600,500);
+//        this.setTitle("Primera Tarea");
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        this.getContentPane().add(panel);
+//        this.setLocationRelativeTo(null);
+//    }
+//
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        main.setVisible(true);
+//    }
+//}
 
---
+++ Mandelbrot/zoom/MandelBrotZoom.java
@@ -0,0 +1,115 @@
+package Mandelbrot.zoom;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MandelBrotZoom extends JFrame {
+    private int _maximoIteraciones;
+    private double xMin =  -2;   // limite real(-2,2)  a
+    private double xMax = 2;
+    private double yMin = -2;  // limite imaginario (-2i,2i)
+    private double yMax = 2;
+
+    private int _ancho = 800;
+    private int _alto = 600;
+
+    private double zoomAcercar = 0.5; // factor de hacer zoom un 50%
+    private boolean estaHaciendoZoom = false;
+
+    public MandelBrotZoom(int cantidadIteraciones) {
+        this._maximoIteraciones = cantidadIteraciones;
+        setTitle("Mandelbrot Básico");
+        setSize(_ancho, _alto);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    estaHaciendoZoom = true;
+                    double x = xMin + (xMax - xMin) * e.getX() / _ancho;
+                    double y = yMin + (yMax - yMin) * e.getY() / _alto;
+                    Acercarse(x, y);
+                    
+                }
+            }
+        });
+
+    }
+
+
+    private void Acercarse(double x, double y) {
+        double nuevoAncho = (xMax - xMin) * zoomAcercar;
+        double nuevoAlto = (yMax - yMin) * zoomAcercar;
+        xMin = x - nuevoAncho / 2;
+        xMax = x + nuevoAncho / 2;
+        yMin = y - nuevoAlto /2;
+        yMax = y + nuevoAlto / 2;
+        repaint();
+    }
+
+
+    private int ejecutarMandelbrot(double x, double y) {
+
+
+        double realCalculado = x;
+
+        double imaginarioCalculado = y;
+
+
+        for (int iteracion = 0; iteracion < _maximoIteraciones; iteracion++) {
+
+            double realCuadrado = realCalculado * realCalculado;
+
+            double imaginarioCuadrado = imaginarioCalculado * imaginarioCalculado;
+
+
+            if (realCuadrado + imaginarioCuadrado  > 4) {
+
+                // - Si la condición anterior es verdadera, se devuelve el número actual de iteraciones.
+                return iteracion;
+            }
+            // Si la condición anterior es falsa, se actualiza el valor de realCalculado.
+
+            realCalculado = realCuadrado - imaginarioCuadrado + x;
+
+            // Se actualiza el valor de imaginarioCalculado.
+            imaginarioCalculado = 2 * realCalculado * imaginarioCalculado + y;
+        }
+
+        return _maximoIteraciones;
+    }
+
+
+    public void paint(Graphics g) {
+
+        // Dibujar el fractal
+
+        for (int x = 0; x < _ancho; x++) { // horizontal o columna x
+            for (int y = 0; y < _alto; y++) { // vertical o fila y
+                double xCalculado = xMin + (xMax - xMin) * x / _ancho;
+                double yCalculado = yMin + (yMax - yMin) * y / _alto;
+
+                int color = ejecutarMandelbrot(xCalculado, yCalculado);
+
+                if (color == _maximoIteraciones) {
+                    g.setColor(Color.white);
+                } else {
+                    g.setColor(Color.black);
+                }
+                g.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new MandelBrotZoom(1000);
+    }
+
+
+}
